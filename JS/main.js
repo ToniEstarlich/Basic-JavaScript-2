1a7ae686f98dfa075e4261ffb133e130701b7357
Antonio Estarlich <110102891+ToniEstarlich@users.noreply.github.com>
2022-10-20 23:15:19 +0100

    Add files via upload

---
+++ JS/main.js
@@ -0,0 +1,9 @@
+function myFuction() { alert(location.hostname);
+}
+document.write("Hello World!"); //Defining a function and naming it
+var blues = "I have the blues."; //Defining a variable and giving it a
+var blues = blues.fontcolor("blue"); //Using the fontcolor method on
+document.write("blue"); //Putting the value
+document.write(3+3); //write an expression
+var Family = "The Arezzinis", Dad = "Jeremiah", Mom = "Hermoine", Daughter = "Penny", Son = "Zorro";
+document.write(Dad); //string values to multiple variables 
